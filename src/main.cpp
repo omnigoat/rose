356f2e81d12bf3500a66db799825b071089590ef
jona <omnigoat@gmail.com>
2013-03-18 14:31:27 +1100

    more testing for refined graph

--- src/main.cpp
+++ src/main.cpp
@@ -52,10 +52,10 @@ auto main() -> int
 */
 	{
 		using namespace sooty::lexing;
-		lexer_t L = match('\n') >> *match('\t') >> !(*match(' ') >> match('\n'));
+		lexer_t L0 = *(match('a', 'z') | match('_')) >> match(' ');
 	}
 
-	std::string input_string = "\nthing\n\tnext_block\n\tsame_block\n\n\tsame_block_still";
+	std::string input_string = "thing 1234 blah_qwer";
 	sooty::lexing::lexemes_t lexemes;
 	sooty::lexing::detail::accumulator_t acc(lexemes, input_string.size());
 	{
@@ -80,25 +80,26 @@ auto main() -> int
 			// identifier
 			insert(2, main_channel, match('a', 'z') >> +(match('a', 'z') | match('_')) ) |
 
-			// operators
-			insert(10, main_channel, match("+")) |
-			insert(11, main_channel, match("-")) |
-			insert(12, main_channel, match("*")) |
-			insert(13, main_channel, match("/")) |
+			//// operators
+			//insert(10, main_channel, match("+")) |
+			//insert(11, main_channel, match("-")) |
+			//insert(12, main_channel, match("*")) |
+			//insert(13, main_channel, match("/")) |
 
-			// brackets
-			insert(20, main_channel, match("(")) |
-			insert(21, main_channel, match(")")) |
+			//// brackets
+			//insert(20, main_channel, match("(")) |
+			//insert(21, main_channel, match(")")) |
 
 			// spaces
-			insert(0, ws_channel, match(' ')) |
+			insert(0, ws_channel, match(' ')) //|
 
+			//match('\n') | match('\t')
 			// tabs/newlines
-			(
+			/*(
 				match('\n') >>
 				*match('\t') [ std::bind(&detail::accumulator_t::inc_tabs, std::placeholders::_1) ] >>
 				!(*match(' ') >> match('\n'))[ std::bind(&detail::accumulator_t::reset_tabs, std::placeholders::_1) ]
-			) [ std::bind(&detail::accumulator_t::blockify, std::placeholders::_1) ]
+			) [ std::bind(&detail::accumulator_t::blockify, std::placeholders::_1) ]*/
 		);
 		
 		lexical_analysis_t()(acc, input_range, BANANA.backend());
